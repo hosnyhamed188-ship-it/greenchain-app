e7dc389c6f74d99145a2635a2bbdd54cd4e9bab5
اسمك <@example.com>
2026-06-22 02:14:57 +0300

    fix supabase import

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://oeojlmldcqzbacdbgpio.supabase.co"
-const supabaseKey = "sb_publishable_g5pgbjRZqfu6U4hP1E82Pg_BD9SLBTv"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
